dd1afd68232d5b082d36006c60902e6b282f47a4
AsenHu <78863300+AsenHu@users.noreply.github.com>
2023-10-04 22:27:51 +0800

    Update dll.sh

--- dll.sh
+++ dll.sh
@@ -32,6 +32,6 @@ install_go_scr() {
     then
         goPath=$(echo "$tmp" |cut -d':' -f2)
         goVer=$(echo "$tmp" |cut -d':' -f3)
-        PATH="$PATH:$goPath"
+        PATH="$PATH:$GO_PATH/bin"
     fi
 }
